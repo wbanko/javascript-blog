8382c458d01c013cbd76aa91a48ba00d84758c06
Weronika Banko <w.b.banko@gmail.com>
2025-04-28 17:55:25 +0200

    add active link

--- js/script.js
+++ js/script.js
@@ -1,23 +1,25 @@
 'use strict';
 
-function titleClickHandler(){
-  console.log('Link was clicked!');
-  console.log(event);
+function titleClickHandler(event){
+    const clickedElement = this;
+    console.log('Link was clicked!');  
   
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
 const activeLinks = document.querySelectorAll('.titles a.active');
 
 for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
 }
-  /* add class 'active' to the clicked link */
-const activeArticles = document.querySelectorAll('.posts article.active')
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+  console.log('clickedElement:', clickedElement);
 
-for(let activeArticle of activeArticles){
-    activeArticle.classList.remove('active');
-}
-  /* remove class 'active' from all articles */
+    clickedElement.classList.add('active');
+  /* [DONE] remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll('.posts article.active')
 
+  for(let activeArticle of activeArticles){
+      activeArticle.classList.remove('active');
+  }
   /* get 'href' attribute from the clicked link */
 
   /* find the correct article using the selector (value of 'href' attribute) */
