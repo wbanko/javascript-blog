ef016b4716187fbfc377c6e986fd61cd4488b9de
Weronika Banko <w.b.banko@gmail.com>
2025-04-29 13:00:55 +0200

    remove titlelist, create loop for each article, get title, create html, insert link

--- js/script.js
+++ js/script.js
@@ -40,3 +40,37 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+/* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+
+  titleList.innerHTML = '';
+  
+/* for each article */
+ const articles = document.querySelectorAll(optArticleSelector);
+
+ for(let article of articles) {
+
+  /* get the article id */
+
+  const articleId = article.getAttribute('id');
+ 
+  /* find the title element */
+  /* get the title from the title element */
+  const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+  
+  /* create HTML of the link */
+  const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+  console.log('Printed title');
+  /* insert link into titleList */
+
+  titleList.insertAdjacentHTML('beforeend', linkHTML);
+ }
+}
+
+generateTitleLinks();
