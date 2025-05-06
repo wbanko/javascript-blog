e3c9cdde9f1b5ad3e1e7dd50e4f2d9efba3fee47
Weronika Banko <w.b.banko@gmail.com>
2025-05-06 22:30:18 +0200

    add Eslint

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
 'use strict';
 
-function titleClickHandler(event){
-    const clickedElement = this;
-    console.log('Link was clicked!');  
+function titleClickHandler(/*event*/){
+  const clickedElement = this;
+  console.log('Link was clicked!');  
   
   /* [DONE] remove class 'active' from all article links  */
-const activeLinks = document.querySelectorAll('.titles a.active');
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
-for(let activeLink of activeLinks){
+  for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
-}
+  }
   /* [DONE] add class 'active' to the clicked link */
   console.log('clickedElement:', clickedElement);
 
-    clickedElement.classList.add('active');
+  clickedElement.classList.add('active');
   /* [DONE] remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.posts article.active')
+  const activeArticles = document.querySelectorAll('.posts article.active');
 
   for(let activeArticle of activeArticles){
-      activeArticle.classList.remove('active');
+    activeArticle.classList.remove('active');
   }
   /* [DONE] get 'href' attribute from the clicked link */
 const articleSelector = clickedElement.getAttribute('href');
